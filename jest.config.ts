cb8ff439e271a25b2556c35ddcfd0721f78334d4
Tomu Maekawa <tomu.m1107@icloud.com>
2025-01-20 09:29:41 +0900

    Revert "Merge pull request #11 from mkw-tom/fix/dockerfile"
    This reverts commit 172c365bf5f4c1727efb5a12de0595262b6993f2, reversing
    changes made to 682246e79735366225e1e8dfd90c92d86603b039.

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,6 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coverageProvider: "v8",
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"], 
   forceExit: true,
 };
 
